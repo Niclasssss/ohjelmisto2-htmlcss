179babc9ec590ef0e4b865394304e4779dbd2c20
Niclasssss <niclas.lausama@kotiposti.net>
2026-03-31 17:56:40 +0300

    commit

--- Moduuli1/Assignment 2/Assignment2.js
+++
@@ -1,3 +0,0 @@
-'use strict';
-const username = prompt('Type your name.')
-document.querySelector('#nimi').innerHTML = 'Hello, ' + username + '.';
--- Moduuli1/Assignment 5/Assignment5.js
+++
@@ -1,7 +0,0 @@
-const vuosi = Number(prompt('Anna vuosi.'))
-
-if (vuosi % 4 == 0 && (vuosi % 100 != 0 || vuosi % 400 == 0)) {
-     document.querySelector('#vuosi').innerHTML = vuosi + ' on karkausvuosi.';
-} else {
-    document.querySelector('#vuosi').innerHTML = vuosi + ' ei ole karkausvuosi.';
-}
